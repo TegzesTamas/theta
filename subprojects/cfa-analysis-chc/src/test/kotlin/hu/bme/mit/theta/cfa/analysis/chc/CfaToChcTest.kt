0999d50a1e0f1dc52229d31bf3d32840e897aa24
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-09-15 08:30:23 -0700

    Rename and add tests

--- subprojects/cfa-analysis-chc/src/test/kotlin/hu/bme/mit/theta/cfa/analysis/chc/CfaToChcTest.kt
+++ subprojects/cfa-analysis-chc/src/test/kotlin/hu/bme/mit/theta/cfa/analysis/chc/CfaToChcTest.kt
@@ -4,6 +4,7 @@ import hu.bme.mit.theta.cfa.CFA
 import hu.bme.mit.theta.cfa.analysis.chc.utilities.DeclManager
 import hu.bme.mit.theta.core.stmt.AssignStmt
 import hu.bme.mit.theta.core.stmt.AssumeStmt
+import hu.bme.mit.theta.core.type.booltype.BoolExprs
 import hu.bme.mit.theta.core.type.inttype.IntExprs
 import hu.bme.mit.theta.core.type.inttype.IntNeqExpr
 import hu.bme.mit.theta.core.utils.PathUtils
@@ -15,7 +16,7 @@ import org.junit.Test
 class CfaToChcTest {
 
     @Test
-    fun LooplessProgramTest() {
+    fun singlePathLooplessSafeTest() {
         val cfaBuilder = CFA.builder()
         val initLoc = cfaBuilder.createLoc("init")
         cfaBuilder.initLoc = initLoc
@@ -43,7 +44,7 @@ class CfaToChcTest {
     }
 
     @Test
-    fun multipathLooplessProgramTest() {
+    fun multiPathLooplessSafeTest() {
         val cfaBuilder = CFA.builder()
         val initLoc = cfaBuilder.createLoc("init")
         cfaBuilder.initLoc = initLoc
@@ -75,6 +76,101 @@ class CfaToChcTest {
             solver.push()
             solver.add(PathUtils.unfold(chc.expr, 0))
             Assert.assertEquals("CHC should be unsatisfiable, but it is satisfiable: $chc", SolverStatus.UNSAT, solver.check())
+            solver.pop()
+        }
+    }
+
+    @Test
+    fun multiPathLooplessUnsafeTest() {
+
+        val cfaBuilder = CFA.builder()
+        val initLoc = cfaBuilder.createLoc("init")
+        cfaBuilder.initLoc = initLoc
+        val errorLoc = cfaBuilder.createLoc("error")
+        cfaBuilder.finalLoc = cfaBuilder.createLoc("final")
+        cfaBuilder.errorLoc = errorLoc
+
+        val midLoc = cfaBuilder.createLoc("middle")
+
+        val x = DeclManager.getVar("CfaToCHCTest_loopless_parallel_x", IntExprs.Int())
+        val y = DeclManager.getVar("CfaToCHCTest_loopless_parallel_y", IntExprs.Int())
+
+
+        cfaBuilder.createEdge(initLoc, midLoc, AssignStmt.of(x, IntExprs.Int(0)))
+        cfaBuilder.createEdge(midLoc, errorLoc, AssumeStmt.of(IntNeqExpr.of(x.ref, IntExprs.Int(0))))
+
+        cfaBuilder.createEdge(initLoc, midLoc, AssignStmt.of(y, IntExprs.Int(0)))
+        cfaBuilder.createEdge(midLoc, errorLoc, AssumeStmt.of(IntNeqExpr.of(y.ref, IntExprs.Int(0))))
+
+
+        val cfa = cfaBuilder.build()
+        val chcs = cfaToChc(cfa)
+
+        Assert.assertEquals("Incorrect number of chc: $chcs", 4, chcs.size)
+
+        val solver = Z3SolverFactory.getInstace().createSolver()
+        var satCount = 0
+        var unsatCount = 0
+        for (chc in chcs) {
+            solver.push()
+            solver.add(PathUtils.unfold(chc.expr, 0))
+            when (solver.check()) {
+                SolverStatus.SAT -> ++satCount
+                SolverStatus.UNSAT -> ++unsatCount
+                else -> {
+                    throw AssertionError("solver.check() returned unknown result")
+                }
+            }
+            solver.pop()
+        }
+        Assert.assertEquals("Incorrect number of CHCs satisfiable", 2, satCount)
+        Assert.assertEquals("Incorrect number of CHCs unsatisfiable", 2, unsatCount)
+    }
+
+    @Test
+    fun singlePathSingleLoopSafeTest() {
+        val cfaBuilder = CFA.builder()
+        val initLoc = cfaBuilder.createLoc("init")
+        cfaBuilder.initLoc = initLoc
+        val errorLoc = cfaBuilder.createLoc("error")
+        cfaBuilder.finalLoc = cfaBuilder.createLoc("final")
+        cfaBuilder.errorLoc = errorLoc
+
+        val midLoc = cfaBuilder.createLoc("middle")
+
+        val x = DeclManager.getVar("CfaToCHCTest_loopless_x", IntExprs.Int())
+        val y = DeclManager.getVar("CfaToCHCTest_loopless_y", IntExprs.Int())
+
+        cfaBuilder.createEdge(initLoc, midLoc, AssignStmt.of(x, IntExprs.Int(0)))
+        cfaBuilder.createEdge(midLoc, errorLoc, AssumeStmt.of(IntNeqExpr.of(x.ref, IntExprs.Int(0))))
+        cfaBuilder.createEdge(midLoc, midLoc, AssignStmt.of(y, IntExprs.Add(y.ref, IntExprs.Int(1))))
+
+        val cfa = cfaBuilder.build()
+
+        val chcs = cfaToChc(cfa)
+
+
+        val solver = Z3SolverFactory.getInstace().createSolver()
+        for (chc in chcs) {
+            solver.push()
+            solver.add(PathUtils.unfold(chc.expr, 0))
+            Assert.assertEquals("CHC unsatisfiable with unbound invariant: $chc", SolverStatus.SAT, solver.check())
+            solver.pop()
+        }
+
+        Assert.assertEquals("Incorrect number of chcs: $chcs", 3, chcs.size)
+        val invariants = chcs.flatMap { it.invariantsToFind }.toSet()
+
+        Assert.assertEquals("Incorrect number of loop invariants: $invariants", 1, invariants.size)
+        val invariantExpr = IntExprs.Eq(x.ref, IntExprs.Int(0))
+        for (chc in chcs) {
+            solver.push()
+            for (invariant in invariants) {
+                solver.add(BoolExprs.Iff(invariant.getConstDecl(0).ref, PathUtils.unfold(invariantExpr, 0)))
+                solver.add(BoolExprs.Iff(invariant.getConstDecl(1).ref, PathUtils.unfold(invariantExpr, chc.postIndexing)))
+            }
+            solver.add(PathUtils.unfold(chc.expr, 0))
+            Assert.assertEquals("CHC satisfiable with bound invariant that should prove safety", SolverStatus.UNSAT, solver.check())
             solver.pop()
         }
     }
