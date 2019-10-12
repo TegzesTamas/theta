5fa368391bed824b73b4ead27809dd907c9b36c6
Tamás Tegzes <tegzes.tamas@gmail.com>
2019-10-12 15:34:01 +0200

    Implement naive top-down chc solving

---
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/NaiveSolver.kt
@@ -0,0 +1,33 @@
+package hu.bme.mit.theta.cfa.analysis.chc
+
+import hu.bme.mit.theta.solver.Solver
+import hu.bme.mit.theta.solver.SolverStatus
+import hu.bme.mit.theta.solver.utils.WithPushPop
+import java.util.*
+
+fun CHCSystem.findFailingPath(solver: Solver): SimpleCHC? {
+    for (simpleCHC in simpleCHCs) {
+        WithPushPop(solver).use {
+            solver.addSimpleCHC(simpleCHC)
+            if (solver.check() == SolverStatus.SAT) {
+                return simpleCHC
+            }
+        }
+    }
+    val chcs = LinkedList<Query>(queries)
+    while (chcs.isNotEmpty()) {
+        val chc = chcs.pop()
+        val toCheck = facts.filter { it.postEndpoint == chc.preEndpoint }.map { it.append(chc) }
+        for (simpleCHC in toCheck) {
+            val res = WithPushPop(solver).use {
+                solver.addSimpleCHC(simpleCHC)
+                solver.check()
+            }
+            if (res == SolverStatus.SAT) {
+                return simpleCHC
+            }
+        }
+        chcs += inductiveClauses.filter { it.postEndpoint == chc.preEndpoint }.map { it.append(chc) }
+    }
+    return null
+}
---
+++ subprojects/cfa-analysis-chc/src/test/kotlin/hu/bme/mit/theta/cfa/analysis/chc/NaiveSolverTest.kt
@@ -0,0 +1,42 @@
+package hu.bme.mit.theta.cfa.analysis.chc
+
+import hu.bme.mit.theta.cfa.CFA
+import hu.bme.mit.theta.cfa.analysis.chc.utilities.DeclManager
+import hu.bme.mit.theta.core.stmt.AssignStmt
+import hu.bme.mit.theta.core.stmt.AssumeStmt
+import hu.bme.mit.theta.core.type.inttype.IntExprs
+import hu.bme.mit.theta.core.type.inttype.IntNeqExpr
+import hu.bme.mit.theta.solver.Solver
+import hu.bme.mit.theta.solver.z3.Z3SolverFactory
+import org.junit.Assert
+import org.junit.Test
+
+class NaiveSolverTest() {
+    private val solver: Solver = Z3SolverFactory.getInstace().createSolver()
+    @Test
+    fun topDownUnsafeTest() {
+        val cfaBuilder = CFA.builder()
+        val initLoc = cfaBuilder.createLoc("init")
+        cfaBuilder.initLoc = initLoc
+        val errorLoc = cfaBuilder.createLoc("error")
+        cfaBuilder.finalLoc = cfaBuilder.createLoc("final")
+        cfaBuilder.errorLoc = errorLoc
+
+        val midLoc = cfaBuilder.createLoc("middle")
+
+        val x = DeclManager.getVar("CfaToCHCTest_loopless_parallel_x", IntExprs.Int())
+        val y = DeclManager.getVar("CfaToCHCTest_loopless_parallel_y", IntExprs.Int())
+
+
+        cfaBuilder.createEdge(initLoc, midLoc, AssignStmt.of(x, IntExprs.Int(0)))
+        cfaBuilder.createEdge(midLoc, errorLoc, AssumeStmt.of(IntNeqExpr.of(x.ref, IntExprs.Int(0))))
+
+        cfaBuilder.createEdge(initLoc, midLoc, AssignStmt.of(y, IntExprs.Int(0)))
+        cfaBuilder.createEdge(midLoc, errorLoc, AssumeStmt.of(IntNeqExpr.of(y.ref, IntExprs.Int(0))))
+
+
+        val cfa = cfaBuilder.build()
+        val chcSystem = cfaToChc(cfa)
+        Assert.assertNotNull(chcSystem.findFailingPath(solver))
+    }
+}
