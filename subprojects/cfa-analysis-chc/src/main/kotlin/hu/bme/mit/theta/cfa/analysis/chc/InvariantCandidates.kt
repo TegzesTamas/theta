e49617f2df389af5090aa45e30973950dc2f6257
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-11-08 17:14:03 +0100

    Remove default constructor parameter from InvariantCandidates

--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/InvariantCandidates.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/InvariantCandidates.kt
@@ -3,7 +3,7 @@ package hu.bme.mit.theta.cfa.analysis.chc
 import hu.bme.mit.theta.core.type.booltype.AndExpr
 import hu.bme.mit.theta.core.type.booltype.OrExpr
 
-data class InvariantCandidates(val default: List<AndExpr>, val candidates: Map<Invariant, List<AndExpr>> = emptyMap()) {
-    operator fun get(invariant: Invariant): List<AndExpr> = candidates[invariant] ?: default
+data class InvariantCandidates(val default: List<AndExpr>, val candidateMap: Map<Invariant, List<AndExpr>>) {
+    operator fun get(invariant: Invariant): List<AndExpr> = candidateMap[invariant] ?: default
     fun getExpr(invariant: Invariant): OrExpr = OrExpr.create(this[invariant])
 }
