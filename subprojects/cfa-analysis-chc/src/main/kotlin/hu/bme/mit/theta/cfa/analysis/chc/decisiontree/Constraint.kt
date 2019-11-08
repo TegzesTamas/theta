1b7d373d9721454ed77834cffecd76d4ce9c168b
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-11-08 17:15:24 +0100

    Make Constraint.target nullable (meaning sources must be false)

--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/decisiontree/Constraint.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/decisiontree/Constraint.kt
@@ -3,7 +3,7 @@ package hu.bme.mit.theta.cfa.analysis.chc.decisiontree
 import hu.bme.mit.theta.cfa.analysis.chc.Invariant
 import hu.bme.mit.theta.core.model.Valuation
 
-data class Constraint(val source: List<Datapoint>, val target: Datapoint) {
+data class Constraint(val source: List<Datapoint>, val target: Datapoint?) {
 
 }
 
