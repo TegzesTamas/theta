8438b574ce91ec11df143aaac5a653b88f98b31a
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-11-18 11:15:01 +0100

    Remove empty class body from Constraint

--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/decisiontree/Constraint.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/decisiontree/Constraint.kt
@@ -3,8 +3,6 @@ package hu.bme.mit.theta.cfa.analysis.chc.decisiontree
 import hu.bme.mit.theta.cfa.analysis.chc.Invariant
 import hu.bme.mit.theta.core.model.Valuation
 
-data class Constraint(val source: List<Datapoint>, val target: Datapoint?) {
-
-}
+data class Constraint(val source: List<Datapoint>, val target: Datapoint?)
 
 data class Datapoint(val invariant: Invariant, val valuation: Valuation)
