68eef9cf157e06e1ab0a8cba5a7d7c84b9df49fa
Tegzes Tamás <tegzes.tamas@gmail.com>
2020-02-19 14:47:27 +0100

    Remove outdated runner script

--- subprojects/cfa-analysis-chc/runner.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-for file in ../../../theta-benchmark/cfa/models/svcomp/loops/*.cfa
-    echo $file
-    timeout 3s java -Djava.library.path="../../lib/" -jar ./build/libs/theta-cfa-analysis-chc-0.0.1-SNAPSHOT-all.jar $file
-    echo "STOP"
-end
