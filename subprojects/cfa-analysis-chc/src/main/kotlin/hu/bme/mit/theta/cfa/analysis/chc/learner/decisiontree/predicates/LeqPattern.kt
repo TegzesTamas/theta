7441313035635d2f858d67bc53f44de80ac4af47
Tegzes Tamás <tegzes.tamas@gmail.com>
2020-10-06 19:16:01 +0200

    Lift decision searching from DecisionTreeLearner to LeqPattern and ListPattern

---
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/decisiontree/predicates/LeqPattern.kt
@@ -0,0 +1,134 @@
+package hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.predicates
+
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.ConstraintSystem
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.Datapoint
+import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ImpurityMeasure
+import hu.bme.mit.theta.core.decl.Decl
+import hu.bme.mit.theta.core.type.Expr
+import hu.bme.mit.theta.core.type.booltype.BoolType
+import hu.bme.mit.theta.core.type.inttype.IntExprs
+import hu.bme.mit.theta.core.type.inttype.IntLitExpr
+import hu.bme.mit.theta.core.type.inttype.IntType
+
+object LeqPattern : PredicatePattern {
+    private data class VariableOccurrence(val variable: Decl<IntType>, val lit: IntLitExpr, val datapoint: Datapoint)
+    private data class ForcedLabeling(val mustBeTrue: Int, val mustBeFalse: Int)
+
+    override fun findBestSplit(datapointsToSplit: Set<Datapoint>, constraintSystem: ConstraintSystem, measure: ImpurityMeasure): PredicatePattern.Split? {
+        val variableToOccurrences = datapointsToSplit.asSequence()
+                .flatMap { datapoint ->
+                    datapoint.valuation.toMap()
+                            .asSequence()
+                            .mapNotNull { (variable, value) ->
+                                (value as? IntLitExpr)?.let {
+                                    @Suppress("UNCHECKED_CAST")
+                                    (VariableOccurrence(variable as Decl<IntType>, it, datapoint))
+                                } //TODO BitVector support
+                            }
+                }
+                .groupBy { it.variable }
+                .asSequence()
+                .map { (variable, occurrences) -> variable to occurrences.groupBy { it.lit } }
+        var bestExpr: Expr<BoolType>? = null
+        var bestError: Double? = null
+
+        for ((variable, occurrences) in variableToOccurrences) {
+            if (occurrences.size > 1) {
+                val bestSplit = findBestSplitForVariable(variable, occurrences,
+                        datapointsToSplit, constraintSystem, measure)
+                if (bestSplit != null) {
+                    val (expr, error) = bestSplit
+                    if (bestError == null || error < bestError) {
+                        bestExpr = expr
+                        bestError = error
+                    }
+                }
+            }
+        }
+        return if (bestError == null || bestExpr == null) {
+            null
+        } else {
+            PredicatePattern.Split(bestExpr, bestError)
+        }
+    }
+
+    private fun findBestSplitForVariable(
+            variable: Decl<IntType>,
+            occurrencesByLit: Map<IntLitExpr, List<VariableOccurrence>>,
+            datapointsToSplit: Set<Datapoint>,
+            constraintSystem: ConstraintSystem,
+            measure: ImpurityMeasure
+    ): Pair<Expr<BoolType>, Double>? {
+        //In splittableDps, every datapoint occurs at most once, because it assigns at most one value to a variable
+        val splittableDps = occurrencesByLit.values.flatMap { it.map { occurrence -> occurrence.datapoint } }
+        val (splittableTrue, splittableFalse) = calcForcedLabeling(splittableDps, constraintSystem)
+        val unsplittableDps = datapointsToSplit - splittableDps
+        val (unsplittableTrue, unsplittableFalse) = calcForcedLabeling(unsplittableDps, constraintSystem)
+
+        var leqMatchingTrue = unsplittableTrue
+        var leqMatchingFalse = unsplittableFalse
+        var matchingTotal = unsplittableDps.size
+        var leqNonMatchingTrue = unsplittableTrue + splittableTrue
+        var leqNonMatchingFalse = unsplittableFalse + splittableFalse
+        var nonMatchingTotal = splittableDps.size + unsplittableDps.size
+
+        var bestExpr: Expr<BoolType>? = null
+        var bestError: Double? = null
+
+
+        val orderedLiterals = occurrencesByLit.entries.sortedBy { it.key.value }
+
+        for ((lit, currentOccurrences) in orderedLiterals) {
+            matchingTotal += currentOccurrences.size
+            nonMatchingTotal -= currentOccurrences.size
+
+            val currentTrue = currentOccurrences.count { it.datapoint in constraintSystem.forcedTrue }
+            val currentFalse = currentOccurrences.count { it.datapoint in constraintSystem.forcedFalse }
+
+            leqMatchingTrue += currentTrue
+            leqNonMatchingTrue -= currentTrue
+            leqMatchingFalse += currentFalse
+            leqNonMatchingFalse -= currentFalse
+
+            val leqError = measure.impurity(leqMatchingTrue, leqMatchingFalse, matchingTotal) +
+                    measure.impurity(leqNonMatchingTrue, leqNonMatchingFalse, nonMatchingTotal)
+
+            if (bestError == null || leqError < bestError) {
+                bestExpr = IntExprs.Leq(variable.ref, lit)
+                bestError = leqError
+            }
+
+            val eqError = measure.impurity(
+                    currentTrue + unsplittableTrue,
+                    currentFalse + unsplittableFalse,
+                    currentOccurrences.size + unsplittableDps.size
+            ) + measure.impurity(
+                    splittableTrue - currentTrue + unsplittableTrue,
+                    splittableFalse - currentFalse + unsplittableFalse,
+                    splittableDps.size - currentOccurrences.size + unsplittableDps.size
+            )
+            if (eqError < bestError) {
+                bestExpr = IntExprs.Eq(variable.ref, lit)
+                bestError = eqError
+            }
+        }
+        return if (bestError == null || bestExpr == null) {
+            null
+        } else {
+            bestExpr to bestError
+        }
+    }
+
+    private fun calcForcedLabeling(dps: Iterable<Datapoint>, constraintSystem: ConstraintSystem): ForcedLabeling {
+        var mustBeTrue = 0
+        var mustBeFalse = 0
+        for (dp in dps) {
+            when (dp) {
+                in constraintSystem.forcedTrue -> ++mustBeTrue
+                in constraintSystem.forcedFalse -> ++mustBeFalse
+            }
+        }
+        return ForcedLabeling(mustBeTrue, mustBeFalse)
+    }
+
+}
---
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/decisiontree/predicates/ListPattern.kt
@@ -0,0 +1,41 @@
+package hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.predicates
+
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.ConstraintSystem
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.Datapoint
+import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ExprDecision
+import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ImpurityMeasure
+import hu.bme.mit.theta.core.type.Expr
+import hu.bme.mit.theta.core.type.booltype.BoolType
+
+data class ListPattern(val atoms: Set<Expr<BoolType>>) : PredicatePattern {
+    override fun findBestSplit(datapointsToSplit: Set<Datapoint>, constraintSystem: ConstraintSystem, measure: ImpurityMeasure): PredicatePattern.Split? {
+        var bestExpr: Expr<BoolType>? = null
+        var bestError: Double? = null
+
+        for (atom in atoms) {
+            val decision = ExprDecision(atom)
+            val trueDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeTrue(it) }
+            val falseDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeFalse(it) }
+            val trueError = measure.impurity(
+                    trueDatapoints.count { constraintSystem.forcedTrue.contains(it) },
+                    trueDatapoints.count { constraintSystem.forcedFalse.contains(it) },
+                    trueDatapoints.count()
+            )
+            val falseError = measure.impurity(
+                    falseDatapoints.count { constraintSystem.forcedTrue.contains(it) },
+                    falseDatapoints.count { constraintSystem.forcedFalse.contains(it) },
+                    falseDatapoints.count()
+            )
+            val newError = trueError + falseError
+            if (bestError == null || newError < bestError) {
+                bestError = newError
+                bestExpr = atom
+            }
+        }
+        return if (bestExpr == null || bestError == null) {
+            null
+        } else {
+            PredicatePattern.Split(bestExpr, bestError)
+        }
+    }
+}
