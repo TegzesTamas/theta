94d8576a17c2d3cf027a8f3116a23a23175eb196
Tegzes Tamás <tegzes.tamas@gmail.com>
2020-10-12 14:32:58 +0200

    Remove Closable interface implementation from Learner

--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/Learner.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/Learner.kt
@@ -2,13 +2,10 @@ package hu.bme.mit.theta.cfa.analysis.chc.learner
 
 import hu.bme.mit.theta.cfa.analysis.chc.InvariantCandidates
 import hu.bme.mit.theta.cfa.analysis.chc.constraint.ConstraintSystem
-import java.io.Closeable
 
-interface Learner : Closeable {
+interface Learner {
     @Throws(CandidatesNotExpressibleException::class)
     fun suggestCandidates(constraintSystem: ConstraintSystem): InvariantCandidates
-
-    override fun close() {}
 
     class CandidatesNotExpressibleException(s: String? = null) : Throwable(s)
 }
