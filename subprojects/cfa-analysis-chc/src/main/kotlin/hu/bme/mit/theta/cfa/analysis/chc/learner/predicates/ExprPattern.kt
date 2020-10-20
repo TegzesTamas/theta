731c94462982550ff0ea414591f1fd089c080b8f
Tegzes Tamás <tegzes.tamas@gmail.com>
2020-10-20 17:50:23 +0200

    Lift ExprPattern from ListPattern

---
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/predicates/ExprPattern.kt
@@ -0,0 +1,38 @@
+package hu.bme.mit.theta.cfa.analysis.chc.learner.predicates
+
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.ConstraintSystem
+import hu.bme.mit.theta.cfa.analysis.chc.constraint.Datapoint
+import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ExprDecision
+import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ImpurityMeasure
+import hu.bme.mit.theta.cfa.analysis.chc.learner.predicates.PredicatePattern.Split
+import hu.bme.mit.theta.core.type.Expr
+import hu.bme.mit.theta.core.type.booltype.BoolType
+import java.util.*
+
+abstract class ExprPattern : PredicatePattern {
+
+    abstract val atoms: Iterable<Expr<BoolType>>
+
+    override fun findAllSplits(datapointsToSplit: Set<Datapoint>, constraintSystem: ConstraintSystem, measure: ImpurityMeasure): PriorityQueue<Split> {
+        val splits = PriorityQueue<Split>()
+
+        for (atom in atoms) {
+            val decision = ExprDecision(atom)
+            val trueDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeTrue(it) }
+            val falseDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeFalse(it) }
+            val trueError = measure.impurity(
+                    trueDatapoints.count { constraintSystem.forcedTrue.contains(it) },
+                    trueDatapoints.count { constraintSystem.forcedFalse.contains(it) },
+                    trueDatapoints.count()
+            )
+            val falseError = measure.impurity(
+                    falseDatapoints.count { constraintSystem.forcedTrue.contains(it) },
+                    falseDatapoints.count { constraintSystem.forcedFalse.contains(it) },
+                    falseDatapoints.count()
+            )
+            val error = trueError + falseError
+            splits.add(Split(atom, error))
+        }
+        return splits
+    }
+}
--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/predicates/ListPattern.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/predicates/ListPattern.kt
@@ -1,36 +1,6 @@
 package hu.bme.mit.theta.cfa.analysis.chc.learner.predicates
 
-import hu.bme.mit.theta.cfa.analysis.chc.constraint.ConstraintSystem
-import hu.bme.mit.theta.cfa.analysis.chc.constraint.Datapoint
-import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ExprDecision
-import hu.bme.mit.theta.cfa.analysis.chc.learner.decisiontree.ImpurityMeasure
-import hu.bme.mit.theta.cfa.analysis.chc.learner.predicates.PredicatePattern.Split
 import hu.bme.mit.theta.core.type.Expr
 import hu.bme.mit.theta.core.type.booltype.BoolType
-import java.util.*
 
-data class ListPattern(val atoms: Set<Expr<BoolType>>) : PredicatePattern {
-
-    override fun findAllSplits(datapointsToSplit: Set<Datapoint>, constraintSystem: ConstraintSystem, measure: ImpurityMeasure): PriorityQueue<Split> {
-        val splits = PriorityQueue<Split>()
-
-        for (atom in atoms) {
-            val decision = ExprDecision(atom)
-            val trueDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeTrue(it) }
-            val falseDatapoints = datapointsToSplit.filterTo(mutableSetOf()) { decision.datapointCanBeFalse(it) }
-            val trueError = measure.impurity(
-                    trueDatapoints.count { constraintSystem.forcedTrue.contains(it) },
-                    trueDatapoints.count { constraintSystem.forcedFalse.contains(it) },
-                    trueDatapoints.count()
-            )
-            val falseError = measure.impurity(
-                    falseDatapoints.count { constraintSystem.forcedTrue.contains(it) },
-                    falseDatapoints.count { constraintSystem.forcedFalse.contains(it) },
-                    falseDatapoints.count()
-            )
-            val error = trueError + falseError
-            splits.add(Split(atom, error))
-        }
-        return splits
-    }
-}
+class ListPattern(override val atoms: Set<Expr<BoolType>>) : ExprPattern()
