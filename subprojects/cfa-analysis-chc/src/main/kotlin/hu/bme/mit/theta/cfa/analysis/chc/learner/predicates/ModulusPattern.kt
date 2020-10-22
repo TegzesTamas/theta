95b667b5d922c1a7fea77a5f131b66716beebc24
Tegzes Tamás <tegzes.tamas@gmail.com>
2020-10-22 15:57:20 +0200

    Fix modulus being negative in ModulusPattern

--- subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/predicates/ModulusPattern.kt
+++ subprojects/cfa-analysis-chc/src/main/kotlin/hu/bme/mit/theta/cfa/analysis/chc/learner/predicates/ModulusPattern.kt
@@ -1,22 +1,23 @@
 package hu.bme.mit.theta.cfa.analysis.chc.learner.predicates
 
 import hu.bme.mit.theta.core.type.Expr
-import hu.bme.mit.theta.core.type.inttype.IntEqExpr
-import hu.bme.mit.theta.core.type.inttype.IntExprs.Eq
-import hu.bme.mit.theta.core.type.inttype.IntExprs.Mod
+import hu.bme.mit.theta.core.type.booltype.BoolExprs.Or
+import hu.bme.mit.theta.core.type.booltype.BoolType
+import hu.bme.mit.theta.core.type.inttype.IntExprs.*
 import hu.bme.mit.theta.core.type.inttype.IntType
 
 class ModulusPattern(intExprs: Iterable<Expr<IntType>>) : ExprPattern() {
-    override val atoms: List<IntEqExpr>
+    override val atoms: List<Expr<BoolType>>
 
     init {
-        val exprs = mutableListOf<IntEqExpr>()
+        val exprs = mutableListOf<Expr<BoolType>>()
 
         for (a in intExprs) {
             for (b in intExprs) {
                 for (c in intExprs) {
-                    // b mod c == a
-                    exprs.add(Eq(Mod(b, c), a))
+                    // c <= 0 || a == (b mod c)
+                    exprs.add(Or(Leq(c, Int(0)),
+                            Eq(a, Mod(b, c))))
                 }
             }
         }
